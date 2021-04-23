2e54a9fc6bbfb29f9399009a6000b741b873907a
anagha778 <anagha778@gmail.com>
2021-04-23 15:23:52 -0400

    portfolio section

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,29 +1,59 @@
 import React from "react";
 
 function Portfolio() {
-  
+  const images = [
+    {
+      img_name:'covid.png',
+      app_name:'Covid Posi+ivity',
+      app_url:'https://uot-project1-group07.github.io/project1-group07/',
+      git_url:'https://github.com/Uot-Project1-Group07/project1-group07'
+    },
+    {
+      img_name:'pim.png',
+      app_name:'MNMLIZE',
+      app_url:'https://mnmlize.herokuapp.com/',
+      git_url:'https://github.com/P-I-M/Personal-Inventory-Management-'
+    },
+    {
+      img_name:'weather.png',
+      app_name:'Weather Dashboard',
+      app_url:'https://anagha778.github.io/weather-dashboard/',
+      git_url:'https://github.com/Anagha778/weather-dashboard'
+    },
+    {
+      img_name:'work.png',
+      app_name:'Work Day Scheduler',
+      app_url:'https://anagha778.github.io/day-scheduler/',
+      git_url:'https://github.com/Anagha778/day-scheduler'
+    },
+    {
+      img_name:'code.png',
+      app_name:'Code Quiz',
+      app_url:'https://anagha778.github.io/code-quiz/',
+      git_url:'https://github.com/Anagha778/code-quiz'
+    },
+    {
+      img_name:'blog.png',
+      app_name:'Tech Blog',
+      app_url:' https://pacific-sands-72490.herokuapp.com/',
+      git_url:'https://github.com/Anagha778/blog-post'
+    },
+
+  ]
+
   return(
   <div>
     <div>
     <h1>Projects</h1>
     </div>
     <div className="flex-row">
-    <figure>
-        <div className="cap"><a href="https://uot-project1-group07.github.io/project1-group07/">Covid Posi+ivity </a><a href="https://github.com/Uot-Project1-Group07/project1-group07"><i className="fa logo">&#xf09b;</i></a>
-        </div>
-        <img src={require(`../../assets/small/covid.png`).default} className="img-thumbnail mx-1" alt="Covid Positivity" key="Covid"/>                
-    </figure>
-    <figure>
-        <div className="cap"><a href="https://mnmlize.herokuapp.com/">MNMLIZE </a><a href="https://github.com/P-I-M/Personal-Inventory-Management-"><i className="fa logo">&#xf09b;</i></a>
-        </div>
-        <img src={require(`../../assets/small/pim.png`).default} className="img-thumbnail mx-1" alt="Mnmlize" key="Mnmlize"/>        
-    </figure>
-    <figure>
-        <div className="cap"> Note taker <a href="https://github.com/Anagha778/note-taker"><i className="fa logo">&#xf09b;</i></a></div>
-        <img src={require(`../../assets/small/covid.png`).default} className="img-thumbnail mx-1" alt="note" key="note"/>        
-    </figure>
-      
-      </div>
+      {images.map(image => (
+      <figure>
+          <div className="cap"><a href={image.app_url} target="_blank">{image.app_name}</a><a href={image.git_url} target="_blank"><i className="fa logo">&#xf09b;</i></a></div>
+          <img src={require(`../../assets/small/${image.img_name}`).default} className="img-thumbnail mx-1" alt={image.app_name} key={image.app_name}/>                
+      </figure>   
+        ))}
+    </div>
   </div>
   );
 };
