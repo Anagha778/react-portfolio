d08badbba94762940b149297ea299ddead2c7273
anagha778 <anagha778@gmail.com>
2021-04-22 23:12:44 -0400

    portfolio 1

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,31 @@
 import React from "react";
 
-const Portfolio = () => (
+function Portfolio() {
+  
+  return(
   <div>
+    <div>
     <h1>Projects</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+    </div>
+    <div className="flex-row">
+    <figure>
+        <div className="cap"><a href="https://uot-project1-group07.github.io/project1-group07/">Covid Posi+ivity </a><a href="https://github.com/Uot-Project1-Group07/project1-group07"><i className="fa logo">&#xf09b;</i></a>
+        </div>
+        <img src={require(`../../assets/small/covid.png`).default} className="img-thumbnail mx-1" alt="Covid Positivity" key="Covid"/>                
+    </figure>
+    <figure>
+        <div className="cap"><a href="https://mnmlize.herokuapp.com/">MNMLIZE </a><a href="https://github.com/P-I-M/Personal-Inventory-Management-"><i className="fa logo">&#xf09b;</i></a>
+        </div>
+        <img src={require(`../../assets/small/pim.png`).default} className="img-thumbnail mx-1" alt="Mnmlize" key="Mnmlize"/>        
+    </figure>
+    <figure>
+        <div className="cap"> Note taker <a href="https://github.com/Anagha778/note-taker"><i className="fa logo">&#xf09b;</i></a></div>
+        <img src={require(`../../assets/small/covid.png`).default} className="img-thumbnail mx-1" alt="note" key="note"/>        
+    </figure>
+      
+      </div>
   </div>
-);
+  );
+};
 
 export default Portfolio;
