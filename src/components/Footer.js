1a2fc02468c235bb55958486ea348a94e0e19ed0
anagha778 <anagha778@gmail.com>
2021-04-24 23:42:54 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,8 @@ function Footer(){
     <a className="lk" href='https://github.com/Anagha778' target="_blank"><i className="fa logo">&#xf09b;</i></a>
     <a className="lk" href='https://www.linkedin.com/' target="_blank"><i className="fa logo">&#xf08c;</i></a>
     <a className="lk" href='https://stackoverflow.com/' target="_blank"><i className="fa logo">&#xf16c;</i></a>
+    <div className="foo">647-321-5900</div>
+    <div className="foo">anagha778@gmail.com</div>
     </div>
     </footer>
     )
